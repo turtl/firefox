8b4ffe7a21981d0a7f718546317d2e66f6c4c19c
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-08-02 12:03:44 -0700

    adding sync acrhitecture, implementing
    - there's now a background sync process that downloads the profile on
      login and continually syncs with the API
    - the sync process has events/tie-ins that other pieces can pull data
      from. for instance, the app tab pulls sync changes from it quite
      successfully.
      the app tab also intializes its data directly from the sync process
      instead of local storage, now.
    - the bookmarker now ties into the sync process as well, although it
      doesnt actually work yet.

--- data/load.js
+++ data/load.js
@@ -1,7 +1,12 @@
-self.port.on('do_load', function(auth, base) {
+self.port.on('do_load', function(auth, base, profile_data) {
 	unsafeWindow._in_ext	=	true;
 	unsafeWindow._auth		=	auth;
 	unsafeWindow._base_url	=	base;
-	unsafeWindow.addon		=	self;
+	unsafeWindow._profile	=	profile_data;
+
+	// disable sync because we'll be sending in updates via the data_handler
+	unsafeWindow.tagit.sync	=	false;
 });
 
+// create a port/event adaptor (Firefox-specific) for the app
+unsafeWindow.port	=	new unsafeWindow.FirefoxAddonPort(self.port);
--- data/sync.js
+++ data/sync.js
@@ -1,2 +1,102 @@
-console.log('auth: ', self.options.auth.key)
+var $E = function(selector, filter){ return ($(filter) || document).getElement(selector); };
+var $ES = function(selector, filter){ return ($(filter) || document).getElements(selector); };
+
+var _in_ext	=	true;
+var port	=	new FirefoxAddonPort(addon.port);
+
+// ------------------------------------------------------------------------------
+// replace a very minimal version of the tagit.js app object for basic syncing
+// purposes. no routing, no pages, etc.
+// ------------------------------------------------------------------------------
+var tagit	=	{
+	// holds the user model
+	user: null,
+
+	// whether or not to sync data w/ server
+	sync: true,
+	sync_timer: null,
+
+	// if true, tells the app to mirror data to local storage
+	mirror: false,
+
+	// -------------------------------------------------------------------------
+	// Data section
+	// -------------------------------------------------------------------------
+	// holds messages for all the user's personas
+	messages: null,
+
+	// holds project/note data for the user
+	profile: null,
+	// -------------------------------------------------------------------------
+
+	init: function(auth)
+	{
+		tagit.user	=	new User();
+		tagit.user.login_from_auth(auth);
+		tagit.load_profile();
+	},
+
+	load_profile: function()
+	{
+		this.api.set_auth(this.user.get_auth());
+		this.messages	=	new Messages();
+		this.profile	=	new Profile();
+		this.profile.initial_load({
+			complete: function() {
+				tagit.profile.persist();
+				tagit.setup_syncing();
+				addon.port.emit('profile-load-complete');
+			}
+		});
+	},
+
+	setup_syncing: function()
+	{
+		tagit.profile.get_sync_time();
+		this.sync_timer = new Timer(10000);
+		this.sync_timer.end = function()
+		{
+			tagit.profile.sync();
+			this.sync_timer.start();
+		}.bind(this);
+		this.sync_timer.start();
+	}
+};
+
+// ------------------------------------------------------------------------------
+// setup event handling for sync script
+// ------------------------------------------------------------------------------
+window.addEvent('domready', function() {
+	addon.port.emit('loaded');
+});
+
+addon.port.on('init', function(user_auth) {
+	window.__site_url		=	window.__site_url || '';
+	window.__api_url		=	window.__api_url || '';
+	window.__api_key		=	window.__api_key || '';
+	tagit.api				=	new Api(
+		__api_url || '',
+		__api_key || '',
+		function(cb_success, cb_fail) {
+			return function(data)
+			{
+				if(typeof(data) == 'string')
+				{
+					data	=	JSON.decode(data);
+				}
+				if(data.__error) cb_fail(data.__error);
+				else cb_success(data);
+			};
+		}
+	);
+	tagit.init(user_auth);
+});
+
+addon.port.on('start', function() {
+	tagit.sync	=	true;
+});
+
+addon.port.on('stop', function() {
+	tagit.sync	=	false;
+});
 
