21dad04056a8011498b965e575c39cd747b2c714
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-08-06 10:57:48 -0500

    fixing menu/reload bugs

--- data/load.js
+++ data/load.js
@@ -1,12 +1,15 @@
-self.port.on('do_load', function(auth, base, profile_data) {
-	unsafeWindow._in_ext	=	true;
-	unsafeWindow._auth		=	auth;
-	unsafeWindow._base_url	=	base;
-	unsafeWindow._profile	=	profile_data;
+if(unsafeWindow.tagit)
+{
+	self.port.on('do_load', function(auth, base, profile_data) {
+		unsafeWindow._in_ext	=	true;
+		unsafeWindow._auth		=	auth;
+		unsafeWindow._base_url	=	base;
+		unsafeWindow._profile	=	profile_data;
 
-	// disable sync because we'll be sending in updates via the data_handler
-	unsafeWindow.tagit.sync	=	false;
-});
+		// disable sync because we'll be sending in updates via the data_handler
+		unsafeWindow.tagit.sync	=	false;
+	});
 
-// create a port/event adaptor (Firefox-specific) for the app
-unsafeWindow.port	=	new unsafeWindow.FirefoxAddonPort(self.port);
+	// create a port/event adaptor (Firefox-specific) for the app
+	unsafeWindow.port	=	new unsafeWindow.FirefoxAddonPort(self.port);
+}
